a30dc3c95549c94343219d34e15b8d4a37369b6a
Andrew Jarrett <ahrjarrett@gmail.com>
2019-04-12 21:53:31 -0500

    add ssh_config file, symlink to .ssh/config

--- bootstrap.exclude.sh
+++ bootstrap.exclude.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-#$PATH_TO_PROJECTS="~/Desktop/stuff"
+#PATH_TO_PROJECTS="~/Desktop/stuff"
+EMAIL="ahrjarrett@gmail.com"
+PATH_TO_GITHUB_KEY="$HOME/.ssh/github"
+FILENAME_SSH_CONFIG="$HOME/.ssh/github"
 
 # Initialize a few things
 init () {
@@ -17,35 +20,44 @@ link () {
   read -s -n 1 input
   if [[ ("$input" = "y") || ("$input" = "Y") || ("$input" = "") ]] ; then
     # TODO - regex here?
-		for file in $( ls -A | grep -vE "\.exclude*|\.git$|\.gitignore$|.*.md$|.*.org$" ) ; do
-			ln -sv "$PWD/$file" "$HOME"
-		done
-		# TODO: source files here?
-		echo "Symlinking complete"
-	else
-		echo "Symlinking cancelled by user"
-		return 1
-	fi
+    for file in $( ls -A | grep -vE "\.exclude*|\.DS_Store|\.git$|\.gitignore$|.*.md$|.*.org$" ) ; do
+      if [[ "$file" = ".ssh_config" ]] ; then
+        mkdir -p "$HOME/.ssh"
+        ln -sv "$PWD/.ssh_config" "$HOME/.ssh"
+      else 
+        ln -sv "$PWD/$file" "$HOME"
+      fi
+    done
+    # TODO: source files here?
+    echo "Symlinking complete"
+  else
+    echo "Symlinking cancelled by user"
+  return 1
+fi
 }
 
 gen_github_ssh_key() {
-  echo "\nThis utility will generate a GitHub SSH key for you"
-  echo "Proceed? (Y/n)"
+  echo "\nThis utility will generate a GitHub SSH key for you at \n"
+  printf "\e[36m  $PATH_TO_GITHUB_KEY  \e[0m\n"
+  echo "\nProceed? (Y/n)"
 		read -s -n 1 input
 		if [[ ("$input" = "y") || ("$input" = "Y") || ("$input" = "") ]] ; then
       echo "Generating GitHub SSH key..."
       echo "Using default key name (github)..."
-      printf "\n\e[32m $HOME/.ssh/github copied to your clipboard \e[0m\n\n"
+      echo "Adding key location to $HOME/.ssh/config..."
+      printf "\n\e[32m$PATH_TO_GITHUB_KEY\e[0m copied to your clipboard \n\n"
 
-      printf $(echo "$HOME/.ssh/github") | pbcopy
-      ssh-keygen -t rsa
-      pbcopy < $HOME/.ssh/github.pub
-      printf "\n\e[32m Contents of $HOME/.ssh/github.pub copied to your clipboard \e[0m\n\n"
+      printf $(echo $PATH_TO_GITHUB_KEY) | pbcopy
+      ssh-keygen -t rsa -C $EMAIL
+      pbcopy < "$PATH_TO_GITHUB_KEY.pub"
+
+      printf "\nContents of \e[32m$PATH_TO_GITHUB_KEY.pub\e[0m copied to your clipboard \n\n"
       echo "Add public key to GitHub"
-      read -p "Press [Enter] key to open GitHub..."
+      read -p $'\e[36mPress [Enter] key to open GitHub →\e[0m'
       open https://github.com/account/ssh
       read -p "Press [Enter] key when finished adding key..."
-      echo "GitHub key generated!"
+      echo "GitHub key generated! To check your config, do:"
+      echo "$ ssh -T git@github.com"
 
     else
       echo "SSH utility cancelled by user"
