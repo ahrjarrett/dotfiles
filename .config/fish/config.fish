cd13e71ff8b41e93fdfde1df805703fa14dbf53b
Andrew Jarrett <ahrjarrett@gmail.com>
2019-04-14 04:29:08 -0500

    pushing latest fish config before breaking into own repo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,24 +31,27 @@ abbr -ag cfb   code $BASHFILE
 abbr -ag cfe   code $EMACS/readme.org
 abbr -ag srcf  source $FISHFILE
 abbr -ag srcb  source $BASHFILE
-abbr -ag fishit  vim $FISH_PATH/config.fish
-abbr -ag bashit  vim $FISH_PATH/config.fish
+abbr -ag fishit   vim $FISH_PATH/config.fish
+abbr -ag bashit   vim $FISH_PATH/config.fish
 
-# filesystem
-abbr -ag ll  'ls -alP'
+# common shell commands
+abbr -ag ll  ls -alP
+abbr -ag c   clear
+abbr -ag sshutdown  sudo shutdown -h now
 
 # git
-abbr -ag ga   git add -A
-abbr -ag gs   git status
-abbr -ag gco  git checkout
-abbr -ag gcm  git commit -m 
+abbr -ag ga     git add -A
+abbr -ag gs     git status
+abbr -ag gco    git checkout
+abbr -ag gcm    git commit -m 
+abbr -ag gpom   git push origin master
 
 # databases
-abbr -ag sqlgo (which mysql).server start
-abbr -ag sqlno (which mysql).server stop
-abbr -ag sqldoh (which mysql).server restart
-abbr -ag pggo launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-abbr -ag pgno launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+abbr -ag sqlgo   (which mysql).server start
+abbr -ag sqlno   (which mysql).server stop
+abbr -ag sqldoh  (which mysql).server restart
+abbr -ag pggo  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+abbr -ag pgno  launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 
 
 #######################
